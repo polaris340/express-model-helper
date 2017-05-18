d93c95e1184a556aaac18aaa8b459d4216f62f64
jiho <polaris341@gmail.com>
2017-05-18 15:13:50 +0900

    오타 수정

--- helper/caseHelper.js
+++ helper/caseHelper.js
@@ -2,7 +2,7 @@ const {memoize} = require('lodash');
 
 const toCamelCase = snake => snake.replace(/(_[a-z])/g, m => m[1].toUpperCase());
 
-exports.toCamelcase = memoize(toCamelcase);
+exports.toCamelcase = memoize(toCamelCase);
 
 exports.toSnakecase = memoize(camel => camel[0].toLowerCase() + camel.substr(1).replace(/([A-Z])/g, m => `_${m[0].toLowerCase()}`));
 
